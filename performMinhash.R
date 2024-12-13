4c98634a84978a11cbe85cdce4ac842e17385455
e13goes <eefje.goes03@gmail.com>
2024-12-13 13:29:40 +0100

    performMinhash.R

--- performMinhash.R
+++ performMinhash.R
@@ -1,3 +1,4 @@
+# Function to perform minhashing
 performMinHash <- function(vectors) {
   nrHashes <- round(0.5*nrow(vectors)/500)*500
   signMatrix <- matrix(Inf, nrow = nrHashes, ncol = ncol(vectors))
@@ -6,14 +7,13 @@ performMinHash <- function(vectors) {
   p <- nextPrime(nrow(vectors))
   a <- sample(0:(p-1), nrHashes)
   b <- sample(1:(p-1), nrHashes)
-
   hash <- matrix(0, nrHashes)
   
-  # Perform MinHashing
   for (r in 1:nrow(vectors)) {
     for (h in 1:nrHashes) {
       hash[h] <- (a[h] + b[h] * r) %% p 
     }
+    
     for (c in 1:ncol(vectors)) {
       if (vectors[r, c] == 1) {
         for (i in 1:nrHashes) {
