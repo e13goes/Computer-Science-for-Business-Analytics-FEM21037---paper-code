38a30748b0ec4743312886d28c078424e4692227
e13goes <eefje.goes03@gmail.com>
2024-12-13 13:21:43 +0100

    extractMW.R

--- extractMW.R
+++ extractMW.R
@@ -1,20 +1,17 @@
-# Regular expression for model words from titles
+# Regular expressions for model words from titles and key-value pairs (new and baseline)
 modelWordTitleRegexNew <- "([a-zA-Z0-9]*(([0-9]+[^0-9, ]+)|([^0-9, ]+[0-9]+))[a-zA-Z0-9]*|\\b\\d+:\\d+\\b|\\b\\d+\\s*x\\s*\\d+\\b)"
 modelWordKeyValueRegexNew <- "(^\\d+(\\.\\d+)?[a-zA-Z]+$|^\\d+(\\.\\d+)?$|\\b\\d+:\\d+\\b|\\b\\d+\\s*x\\s*\\d+\\b)"
-
 modelWordTitleRegex <- "([a-zA-Z0-9]*(([0-9]+[^0-9, ]+)|([^0-9, ]+[0-9]+))[a-zA-Z0-9]*)"
 modelWordKeyValueRegex <- "(^\\d+(\\.\\d+)?[a-zA-Z]+$|^\\d+(\\.\\d+)?$)"
 
+# Function to extract model words 
 extractModelWords <- function(data, regex) {
-  # Apply the regex to extract model words
   words <- str_extract_all(data, regex)
-  
-  # Flatten the list and remove duplicates
   unique(unlist(words))
 }
 
+# Function to remove non-numerical part of model words
 normalizeModelWords <- function(modelWords) {
-  # Remove non-numeric parts (e.g., 'lbs' from '20.8lbs')
   sapply(modelWords, function(word) {
     if (str_detect(word, "\\d+[a-zA-Z]+$")) {
       str_extract(word, "^\\d+(\\.\\d+)?")
@@ -24,16 +21,15 @@ normalizeModelWords <- function(modelWords) {
   })
 }
 
-initializeModelWordSetsBaseline <- function(data) {
+# Function to get model words based on baseline method
+getModelWordsBaseline <- function(data) {
   MWtitle <- c()
   MWvalue <- c()
   
   for (product in data) {
-    # Extract model words from title
     titleWords <- extractModelWords(product$title, modelWordTitleRegex)
     MWtitle <- unique(c(MWtitle, titleWords))
     
-    # Extract and normalize model words from key-value pairs
     for (key in names(product$featuresMap)) {
       value <- product$featuresMap[[key]]
       valueWords <- extractModelWords(value, modelWordKeyValueRegex)
@@ -41,20 +37,18 @@ initializeModelWordSetsBaseline <- function(data) {
       MWvalue <- unique(c(MWvalue, valueWords))
     }
   }
-  
   list(MWtitle = MWtitle, MWvalue = MWvalue)
 }
 
-initializeModelWordSetsNew <- function(data) {
+# Function to get model words based on new method
+getModelWordsNew <- function(data) {
   MWtitle <- c()
   MWvalue <- c()
   
   for (product in data) {
-    # Extract model words from title
     titleWords <- extractModelWords(product$title, modelWordTitleRegexNew)
     MWtitle <- unique(c(MWtitle, titleWords))
     
-    # Extract and normalize model words from key-value pairs
     for (key in names(product$featuresMap)) {
       value <- product$featuresMap[[key]]
       valueWords <- extractModelWords(value, modelWordKeyValueRegexNew)
@@ -62,6 +56,5 @@ initializeModelWordSetsNew <- function(data) {
       MWvalue <- unique(c(MWvalue, valueWords))
     }
   }
-  
   list(MWtitle = MWtitle, MWvalue = MWvalue)
 }
