e1c4b7c4b8b614684e6b1b449bb7f83873a9a494
e13goes <eefje.goes03@gmail.com>
2024-12-13 13:25:43 +0100

    metricsLSH.R

--- metricsLSH.R
+++ metricsLSH.R
@@ -1,6 +1,5 @@
 # Function to compute evaluation metrics
 metricsLSH <- function(data, trueDuplicates, candidatePairs) {
-  
   duplicatesFound <- length(intersect(trueDuplicates, candidatePairs))
   totalDuplicates <- length(trueDuplicates)
   totalPredictedDuplicates <- length(candidatePairs)
@@ -9,7 +8,6 @@ metricsLSH <- function(data, trueDuplicates, candidatePairs) {
   pairQuality <- duplicatesFound / totalComparisons
   pairCompleteness <- duplicatesFound / totalDuplicates
   f1Star <- (2 * pairQuality * pairCompleteness) / (pairQuality + pairCompleteness)
-  
   fracComparisons <- totalComparisons/choose(length(data),2)
   
   return(c("Pair Quality" = pairQuality, "Pair Completeness" = pairCompleteness, "F1*-Measure" = f1Star, "Fraction Comparisons" = fracComparisons))
