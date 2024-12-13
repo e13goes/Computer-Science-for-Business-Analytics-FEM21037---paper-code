ef10cb585b70e4c4392d57f3670ff8c8dab094e4
e13goes <eefje.goes03@gmail.com>
2024-12-13 13:10:27 +0100

    createBinaryVectors.R

--- createBinaryVectors.R
+++ createBinaryVectors.R
@@ -1,23 +1,20 @@
+# Function to create characteristic vectors
 createBinaryVectors <- function(data, MWtitle, MWvalue) {
   allModelWords <- unique(c(MWtitle, MWvalue))
   binaryVectors <- matrix(0, nrow = length(allModelWords), ncol = length(data))
   rownames(binaryVectors) <- allModelWords
   
   for (i in seq_along(data)) {
-
     product <- data[[i]]
-    
-    # Extract model words from title
     titleWords <- extractModelWords(product$title, modelWordTitleRegex)
-    
-    # Extract and normalize model words from featuresMap
     valueWords <- c()
+    
     for (key in names(product$featuresMap)) {
       value <- product$featuresMap[[key]]
       valueWords <- extractModelWords(value, modelWordKeyValueRegex)
     }
-    valueWords <- normalizeModelWords(valueWords)
     
+    valueWords <- normalizeModelWords(valueWords)
     productWords <- unique(c(titleWords, valueWords))
     binaryVectors[rownames(binaryVectors) %in% productWords, i] <- 1
   }
